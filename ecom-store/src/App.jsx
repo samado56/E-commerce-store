f857a56caad87c45ba482ca136227c0cbf1c34aa
samado56 <kamaljamil56@gmail.com>
2025-04-15 19:35:13 +0100

    scaffolding fronted

---
+++ ecom-store/src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
